3c63ba1e85b25fad37994ce5b27e138bbd4b584a
tatsuma.kato <ec2-user@ip-172-31-22-12.us-east-2.compute.internal>
2018-06-01 01:43:00 +0000

    validation

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -2,6 +2,17 @@
 
 @section('content')
 
-<!-- Write content for each page here -->
+    <h1>メッセージ新規作成ページ</h1>
+
+
+
+    {!! Form::model($task, ['route' => 'tasks.store']) !!}
+
+        {!! Form::label('content', 'メッセージ:') !!}
+        {!! Form::text('content') !!}
+
+        {!! Form::submit('投稿') !!}
+
+    {!! Form::close() !!}
 
 @endsection
--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -2,6 +2,18 @@
 
 @section('content')
 
-<!-- Write content for each page here -->
+    <h1>id: {{ $task->id }} のメッセージ編集ページ</h1>
+
+
+
+
+    {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
+
+        {!! Form::label('content', 'メッセージ:') !!}
+        {!! Form::text('content') !!}
+
+        {!! Form::submit('更新') !!}
+
+    {!! Form::close() !!}
 
 @endsection
